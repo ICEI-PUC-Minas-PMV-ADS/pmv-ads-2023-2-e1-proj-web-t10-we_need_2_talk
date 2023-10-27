9464839f67a771a3a8e1e57342697bd62c8b110a
vitor4949 <vitorrs4949@gmail.com>
2023-10-27 13:41:32 -0300

    feedback

---
+++ codigo-fonte/src/paginaFeedback/js/main.js
@@ -0,0 +1,60 @@
+let levelZero = document.getElementById('level-zero'); 
+let levelOne = document.getElementById('level-um');
+let levelTwo = document.getElementById('level-dois');
+let levelThree = document.getElementById('level-tres');
+let levelFour = document.getElementById('level-quatro');
+let levelFive = document.getElementById('level-cinco');
+
+function level(level) {
+    switch(level) {
+        case "level-zero":
+            levelOne.checked = false;
+            levelTwo.checked = false;
+            levelThree.checked = false;
+            levelFour.checked = false;
+            levelFive.checked = false;
+            break;
+        case "level-um":
+            levelZero.checked = true;
+            levelTwo.checked = false;
+            levelThree.checked = false;
+            levelFour.checked = false;
+            break;
+        case "level-dois":
+            levelZero.checked = true;
+            levelOne.checked = true;
+            levelThree.checked = false;
+            levelFour.checked = false;
+            levelFive.checked = false;
+            break;
+        case "level-tres":
+            levelZero.checked = true;
+            levelOne.checked = true;
+            levelTwo.checked = true;
+            levelFour.checked = false;
+            levelFive.checked = false;
+            break;
+        case "level-quatro": 
+            levelZero.checked = true;
+            levelOne.checked = true;
+            levelTwo.checked = true;
+            levelThree.checked = true;
+            levelFive.checked = false;
+            break;
+        case "level-cinco":
+            levelZero.checked = true;
+            levelOne.checked = true;
+            levelTwo.checked = true;
+            levelThree.checked = true;
+            levelFour.checked = true;
+            
+            break;
+    }
+    
+}
+
+let Enviar = document.getElementById('Enviar');
+
+Enviar.onclick() {
+    alert("Enviado com sucesso!");
+}
