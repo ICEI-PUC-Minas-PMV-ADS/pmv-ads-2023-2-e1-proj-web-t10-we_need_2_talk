7b627a7f976278166aa9e5cb658fa8310397f8bc
Emilia-ads <144388125+Emilia-ads@users.noreply.github.com>
2023-11-26 20:49:11 -0300

    Add files via upload

---
+++ codigo-fonte/src/paginaDesempenho/quizespanhol.js
@@ -0,0 +1,151 @@
+const $startGameButton = document.querySelector(".start-quiz")
+const $nextQuestionButton = document.querySelector(".next-question")
+const $questionsContainer = document.querySelector(".questions-container")
+const $questionText = document.querySelector(".question")
+const $answersContainer = document.querySelector(".answers-container")
+const $answers = document.querySelectorAll(".answer")
+
+let currentQuestionIndex = 0
+let totalCorrect = 0
+
+$startGameButton.addEventListener("click", startGame)
+$nextQuestionButton.addEventListener("click", displayNextQuestion)
+
+function startGame() {
+  $startGameButton.classList.add("hide")
+  $questionsContainer.classList.remove("hide")
+  displayNextQuestion()
+}
+
+function displayNextQuestion() {
+  resetState()
+  
+  if (questions.length === currentQuestionIndex) {
+    return finishGame()
+  }
+
+  $questionText.textContent = questions[currentQuestionIndex].question
+  questions[currentQuestionIndex].answers.forEach(answer => {
+    const newAsnwer = document.createElement("button")
+    newAsnwer.classList.add("button", "answer")
+    newAsnwer.textContent = answer.text
+    if (answer.correct) {
+      newAsnwer.dataset.correct = answer.correct
+    }
+    $answersContainer.appendChild(newAsnwer)
+
+    newAsnwer.addEventListener("click", selectAnswer)
+  })
+}
+
+function resetState() {
+  while($answersContainer.firstChild) {
+    $answersContainer.removeChild($answersContainer.firstChild)
+  }
+
+  document.body.removeAttribute("class")
+  $nextQuestionButton.classList.add("hide")
+}
+
+function selectAnswer(event) {
+  const answerClicked = event.target
+
+  if (answerClicked.dataset.correct) {
+    document.body.classList.add("correct")
+    totalCorrect++
+  } else {
+    document.body.classList.add("incorrect") 
+  }
+
+  document.querySelectorAll(".answer").forEach(button => {
+    button.disabled = true
+
+    if (button.dataset.correct) {
+      button.classList.add("correct")
+    } else {
+      button.classList.add("incorrect")
+    }
+  })
+  
+  $nextQuestionButton.classList.remove("hide")
+  currentQuestionIndex++
+}
+
+function finishGame() {
+  const totalQuestions = questions.length
+  const performance = Math.floor(totalCorrect * 100 / totalQuestions)
+  
+  let message = ""
+
+  switch (true) {
+    case (performance >= 90):
+      message = "Excelente :)"
+      break
+    case (performance >= 70):
+      message = "Muito bom :)"
+      break
+    case (performance >= 50):
+      message = "Bom"
+      break
+    default:
+      message = "Pode melhorar :("
+  }
+
+  $questionsContainer.innerHTML = 
+  `
+    <p class="final-message">
+      Você acertou ${totalCorrect} de ${totalQuestions} questões!
+      <span>Resultado: ${message}</span>
+    </p>
+    <button 
+      onclick=window.location.reload() 
+      class="button"
+    >
+      Refazer teste
+    </button>
+  `
+}
+
+
+const questions = [
+  {
+    question: "Mi hermano es______alto.",
+    answers: [
+      { text: "muy", correct: true },
+      { text: "mucha", correct: false },
+      { text: "mucho", correct: false },
+    ],
+  },
+  {
+    question: "Tengo________hambre.",
+    answers: [
+      { text: "mucho", correct: false },
+      { text: "mucha", correct: true },
+      { text: "muy", correct: false },
+    ],
+  },
+  {
+    question: "Yo haré la cena ________tú bañas a los niños.",
+    answers: [
+      { text: "porque", correct: false },
+      { text: "pero", correct: false },
+      { text: "mientras", correct: true },
+    ],
+  },
+  {
+    question: "______ voy a continuar trabajando en la misma empresa.",
+    answers: [
+      { text: "pero", correct: false },
+      { text: "ni", correct: false },
+      { text: "por el momento", correct: true },
+    ],
+  },
+  {
+    question: "Mi hermano compró un______muy caro..",
+    answers: [
+      { text: "domingo", correct: false },
+      { text: "calles", correct: false },
+      { text: "coche", correct: true },
+    ],
+  },
+]
